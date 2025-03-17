e1530aae44c8e1214c1c49695e9b8152ad9d71a8
108-0 <jeong5ac1@g.shingu.ac.kr>
2025-03-17 15:43:51 +0900

    붓꽃 분류기 완성본

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from starlette.middleware.cors import CORSMiddleware
 
 from irisModel import IrisMachineLearning, IrisSpecies
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+)
+
 
 model = IrisMachineLearning()
 
