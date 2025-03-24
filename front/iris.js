f1d1c5ec14698fc470460bb1e0c2720df436f0cd
108-0 <jeong5ac1@g.shingu.ac.kr>
2025-03-24 11:27:30 +0900

    프로젝트 종료

--- front/iris.js
+++ front/iris.js
@@ -1,30 +1,54 @@
-function Send(){
-    s1 = document.getElementById("sl")
-    sw = document.getElementById("sw")
-    pl = document.getElementById("pl")
-    pw = document.getElementById("pw")
+google.charts.load('current', {'packages':['corechart']});
+
+function drawChart(prob, pid) {
+        var data = google.visualization.arrayToDataTable([
+          ['iris species', 'prob'],
+          ['setosa',     prob[0][0]],
+          ['versicolor',     prob[0][1]],
+          ['virginica',  prob[0][2]],
+        ]);
+
+        var options = {
+          title: '붓꽃 품종 확률',
+            pieHole:0.4,
+        };
+
+        var chart = new google.visualization.PieChart(pid);
+
+        chart.draw(data, options);
+      }
+
+
+function Send()
+{
+    sl = document.getElementById('sl').value
+    sw = document.getElementById('sw').value
+    pl = document.getElementById('pl').value
+    pw = document.getElementById('pw').value
 
     var data = {
-        "sepal_length" : sl.value,
-        "sepal_width" : sw.value,
-        "petal_length" : pl.value,
-        "petal_width": pw.value
+        'sepal_length' : sl,
+        'sepal_width' : sw,
+        'petal_length' : pl,
+        'petal_width' : pw
     }
 
     $.ajax({
-       type: "POST",
+        type: "POST",
         url: "http://localhost:8000/predict",
-        headers:{
-            "Accept": "application/json",
-            "Content-Type": "application/json",
+        headers: {
+            'Content-Type': 'application/json'
         },
-        data: JSON.stringify(data),
-       }).done(function(response){
-            console.log(response)
-            txtOut.value = response.prediction + " 일 확률:" + response.probability
+        data : JSON.stringify(data),
+        dataType: 'json'
 
-       }).fail(function(response){
-            alert("fail" + response)
-       }
-       ).always()
+    }).done(function(response) {
+        console.log(response)
+        txtOut.value = response.prediction + "일 확률: " + response.probability
+        google.charts.setOnLoadCallback(drawChart(response.probability, document.getElementById("donutchart") ));
+    }).fail(function(response) {
+        alert("fail" + JSON.stringify(response))
+    }).always(function() {
+
+    })
 }
